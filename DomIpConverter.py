42b00f78f9b2265c1a4e7cf5e112364246b4e395
Setttop <72707415+Setop21@users.noreply.github.com>
2021-06-10 19:25:01 +0200

    Update DomIpConverter.py

--- DomIpConverter.py
+++ DomIpConverter.py
@@ -4,8 +4,7 @@ import socket, os
 file = open('results.txt','w')
 lignes = [ligne.rstrip('\n') for ligne in open('source.txt')]
 for ligne in lignes:
-        #print("Primary IP address, Alias names and other IP addresses of the host name {}: {}".format(ligne, ipAddress))
-        #print('Ip du domaine'+' '+ ligne + '->' + socket.gethostbyname_ex(dom))
+        
     try: 
         ipAddress   = socket.gethostbyname_ex(ligne)
         file.write("{}".format(ipAddress)+'\n')
